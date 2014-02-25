54d52162c61c3247e96d4f1cf32e91ef32d70839
Mark Cotton <tifosigoon@gmail.com>
2014-02-25 13:10:44 -0600

    started repo over after pushing password in config.js

---
+++ README.md
@@ -0,0 +1,33 @@
+####Watcher for Eagle Eye Networks Poll Stream####
+
+This is a Node.js client that follows the EEVBS API.  It listens for new preview images and then sends them to the client through [Socket.io](http://socket.io).  This is a great place to start if you want to strip the auth requirement for the images and make them available to less capable clients (phones, tvs, pictures frames, etc)  It is also a good place to see a simplified version of subscribing to events.
+
+#####Lifecycle of the app#####
+The following steps need to be performed in order, but any call can be made once user is logged-in.
+
+ - Login (step 1)
+ - Login (step 2)
+ - Current user's information is returned by Login (step 2)
+ - Get device list
+ - Subscribe to poll stream
+ - Get subsequent events from poll stream
+
+#####Installation#####
+
+ - install [Node.js](http://nodejs.org)
+ - run `npm install	` 
+ - run `npm start	`
+ - go to [http://localhost:3000](http://localhost:3000)
+
+
+#####Configure#####
+
+ Edit `config.js` and replace 'your_username' and 'your_password' with your username and password.
+
+
+        module.exports = {
+            'username'  :   'your_username',
+            'password'  :   'your_password'
+        }
+
+
---
+++ app.js
@@ -0,0 +1,289 @@
+var r           =       require('request'),
+    u           =       require('underscore')._,
+    router      =       require('router'),
+    repl        =       require('repl');
+
+var route       =       router();
+
+var app         =       require('http').createServer(route),
+    io          =       require('socket.io').listen(app, { log: false }),
+    fs          =       require('fs');
+
+    app.listen(3000);
+
+    
+var  out        =       console.log,
+     username   =       require('./config').username || 'username',
+     password   =       require('./config').password || 'password',
+     realm      =       'eagleeyenetworks',
+     host       =       'https://eagleeyenetworks.com';
+
+var user        =       {},
+    devices     =       {
+                            cameras: [],
+                            bridges: []
+                        };
+
+var debug = function(arg) {
+    repl.start({
+      prompt: "node via stdin> ",
+      input: process.stdin,
+      output: process.stdout
+    }).context.arg = arg;
+};
+
+
+
+function startUp(success, failure) {
+    out('**********************************');
+    out('           Starting up            ');
+    out('**********************************');
+    if ( typeof success === 'function') success(); // call success callback
+}
+
+function login(success, failure) {
+    r.post({
+            url: host + '/g/aaa/authenticate', 
+            json: true, 
+            body: { 'username': username, 'password': password, 'realm': realm }
+            }, function(err, res, body) {
+                if (err) { out(err.stack); }
+                if (!err) {
+                    switch(res.statusCode) {
+                        case 200:
+                            r.post({ url: host + '/g/aaa/authorize', json: true, body: { token: res.body.token } }, function(err, res, body) {
+                               if (err) { out(err.stack); }
+                               if (!err && res.statusCode == 200) { 
+                                    out('**********************************');
+                                    out('           Logged in              ');
+                                    out('**********************************');
+                                    user = res.body;
+                                    if ( typeof success === 'function') success(); // call success callback
+                                }
+                            }) 
+                            break;
+                        default:
+                            out(res.statusCode + ': ' +  res.body);
+                            if ( typeof failure === 'function') failure(); // call failure callback
+
+                    }
+                }    
+    })
+
+}
+
+function getDevices(success, failure) {
+    r.get({url: host + '/g/list/devices', json: true }, function(err, res, body) {
+        if (err) { out(err.stack) }
+        if (!err && res.statusCode == 200) {
+            out('**********************************');
+            out('           Grabbed Devices        ');
+            out('**********************************');
+
+            u.each(res.body, function(device) {
+                var tmp = {};                
+                if(device[3] === 'camera') {
+                    tmp = {
+                        deviceID:           device[1] || '',
+                        deviceStatus:       device[5] || ''
+                    };
+                    devices.cameras.push(tmp);
+                } else {
+                    tmp = {
+                        deviceID:           device[1] || '',
+                        deviceStatus:       device[5] || ''
+                    }; 
+                    devices.bridges.push(tmp);
+                }
+            });
+            
+            if ( typeof success === 'function') success(); // call success callback
+        }
+    });
+}
+
+function startPolling(socket) {
+    var obj = { 'cameras': {} };
+   
+    u.each(u.filter(devices.bridges, function(item) { return item.deviceStatus === 'ATTD'; } ), function(item) {
+        obj.cameras[item.deviceID] = { "resource": ["event"], "event": ["ALLL"] };
+    });
+
+    u.each(u.filter(devices.cameras, function(item) { return item.deviceStatus === 'ATTD'; } ), function(item) {
+        obj.cameras[item.deviceID] = { "resource": ["pre", "thumb", "event", "video"], "event": ["ALLL"] };
+    });
+
+    out('**********************************');
+    out('           Start Polling          ');
+    out('**********************************');
+
+    r.post({
+            url:    host + '/poll',
+            json:   true,
+            body:   JSON.stringify( obj) 
+           }, function(err, res, body) {
+                if (err) { out(err.stack) };
+                if (!err) {
+                    switch(res.statusCode) {
+                        case 200:
+                            keepPolling(socket);
+                            break;
+                         default:
+                            out(res.statusCode);
+                            out('**********************************');
+                            out('           Restart Polling        ');
+                            out('**********************************');
+                            startPolling(); 
+                            break;
+                    }
+                }
+
+    });
+
+
+}
+
+function keepPolling(socket) {
+    //out('**********************************');
+    //out('           Keep Polling           ');
+    //out('**********************************');
+
+    r.get({
+            url:    host + '/poll',
+            json:   true,
+           }, function(err, res, body) {
+                if (err) { out(err.stack) };
+                if (!err) {
+                    switch(res.statusCode) {
+                        case 200:
+                            // got a valid polling cookie
+                            processPollingData(socket, res.body);
+                            keepPolling(socket);
+                            break;
+                        case 400:
+                            // got an invalid polling cookie
+                            //debug({ 'res': res, 'socket': socket });
+                            break;
+                        default:
+                            out(res.statusCode);
+                            out('**********************************');
+                            out('           Restart Polling        ');
+                            out('**********************************');
+                            startPolling(socket); 
+                            break;
+                    }
+                }
+
+    });
+
+}
+
+function processPollingData(socket, data) {
+    //out('**********************************');
+    //out('           Processing Data        ');
+    //out('**********************************');
+    //out(data);
+    socket.emit('poll', { data: data });
+}
+
+
+io.sockets.on('connection', function (socket) {
+
+    // tell the client what there id is
+    socket.send(socket.id);
+    out('Client\'s socket id is: ' + socket.id);
+
+    startUp( function() {
+        login( function() {
+            getDevices(function() {
+                startPolling(socket)
+            });
+        },
+        // failure case for login
+        function() {
+            console.log('Failed to login using these credentials  ' + username + ' : ' + password );    
+        });
+    });
+});
+
+io.sockets.on('disconnect', function(socket) {
+    out('socket disconnected', socket);
+});
+
+route.get('/image/{device}/{ts}', function(orig_req, orig_res) {
+    var ts      =   orig_req.params.ts,
+        device  =   orig_req.params.device;
+
+    //console.log('DEBUG: matching /image/' + device + '/' + ts);
+
+    ts = (ts.indexOf('now') >= -1) ? 'now' : ts;
+
+    var url = host + '/asset/prev/image.jpeg?c=' + device + ';t=' + ts + ';q=high;a=pre';
+
+    try {
+        r.get(url).pipe(orig_res)
+    } catch(e) {
+        out('error in fetching images: ', e)
+    }
+
+});
+
+
+route.get('/jquery.preview.js', function(req, res) {
+  fs.readFile(__dirname + '/jquery.preview.js',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading /jquery.preview.js');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+    
+    out('serving /jquery.preview.js');
+
+  });
+});
+
+route.get('/page.html', function(req, res) {
+  fs.readFile(__dirname + '/page.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading page.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+    
+    out('serving /page.html');
+
+  });
+});
+
+route.get('*', function(req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+    
+    out('serving /index.html');
+
+  });
+});
+
+
+
+process.on('uncaughtException', function(err) {
+  console.log('Caught exception: ' + err);
+  switch(err) {
+    case 'Error: Parse Error':
+        out(err);
+        break;
+  }
+});
